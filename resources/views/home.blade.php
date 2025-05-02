57f49a76ed0f6e6cb4a7c7f07a2533afc7793922
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-05-02 23:40:11 +0100

    laravel initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="bg-blue-200" >
+    Welcomeee
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom-purple': '#F0EBFD',
+                'custom-dark-blue': '#2D2244',
+                'custom-purple-2': '#A88EEE',
+                'custom-purple-3': '#9271EA',
+                
+              }
+        },
+    },
+    plugins: [],
+};
