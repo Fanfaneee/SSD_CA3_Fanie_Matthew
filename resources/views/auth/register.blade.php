17c4f31faf775b0aeadbaa9bd8bf8ce04aba7da1
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-05-04 14:23:32 +0100

    change color login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,49 +2,48 @@
 
 @section('content')
 <div class="container">
-    <h1>Register Blade View is working!</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card bg-dark text-white"> <!-- Ajout de bg-dark et text-white -->
+                <div class="card-header text-white">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="mb-3">
-                            <label for="name" class="form-label">{{ __('Name') }}</label>
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <label for="name" class="form-label text-white">{{ __('Name') }}</label>
+                            <input id="name" type="text" class="form-control text-black @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                             @error('name')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback text-white" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
 
                         <div class="mb-3">
-                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <label for="email" class="form-label text-white">{{ __('Email Address') }}</label>
+                            <input id="email" type="email" class="form-control text-black @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                             @error('email')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback text-white" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
 
                         <div class="mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <label for="password" class="form-label text-white">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control text-black @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                             @error('password')
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback text-white" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
 
                         <div class="mb-3">
-                            <label for="password-confirm" class="form-label">{{ __('Confirm Password') }}</label>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <label for="password-confirm" class="form-label text-white">{{ __('Confirm Password') }}</label>
+                            <input id="password-confirm" type="password" class="form-control text-black" name="password_confirmation" required autocomplete="new-password">
                         </div>
 
                         <div class="mb-0">
